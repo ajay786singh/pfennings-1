596104bb83132b113c2e191d3f657c788ad3370b
Lionel Mann <lionelmann@gmail.com>
2015-01-27 16:13:31 -0500

    homepage template

--- wordpress/wp-content/themes/pfennings/front-page.php
+++ wordpress/wp-content/themes/pfennings/front-page.php
@@ -1,12 +1,32 @@
 <?php get_header(); ?>
 
-<section role="slider">
-	<header>
-		<hgroup>
-			<h6 class="headline"><strong>Howdy.</strong> Pfenning's Organic Farm is a <a href="#">grower</a>/<a href="#">packer</a>/<a href="#">shipper</a> of <a href="#">certified organic produce</a>. To us, 
-			Organic isn't just a certification-it's a <a href="#">way of being</a>. <a href="#">A mindset. And a mission.</a> <br><a href="#"><b>Let's talk!</b></a></h6>
-		</hgroup>
-	</header>
+<!-- Loop starts -->
+<?php if(have_posts()):while(have_posts()):the_post();?>
+
+<!-- Get custom meta values -->
+<?php 
+    $bannerHeadline     = get_post_meta($post->ID,'_banner_heading',true);
+    $bannerImageId      = get_post_meta($post->ID, '_banner_image', true);
+    $bannerImageUrl     = wp_get_attachment_image_src($bannerImageId,'banner', true);
+?>
+
+<section role="slider" style="background: url(<?php echo $bannerImageUrl[0]; ?>) no-repeat; background-size: cover; background-position: center center;">
+    <header>
+        <hgroup>
+            <h6 class="headline"> 
+                <?php if ($bannerHeadline) { ?>
+                    <?php echo $bannerHeadline; ?>
+                <?php } else { ?>
+                    <?php echo the_title(); ?>
+                <?php  } ?> 
+            </h6>
+        </hgroup>
+    </header>
 </section>
+
+<?php 
+    endwhile; 
+    endif;
+?>
 
 <?php get_footer(); ?>

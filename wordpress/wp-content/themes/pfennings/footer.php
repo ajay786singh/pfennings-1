025ac80c163873664eb0b33073333b314206a129
Lionel Mann <lionelmann@gmail.com>
2015-01-28 19:35:18 -0500

    footer fix

--- wordpress/wp-content/themes/pfennings/footer.php
+++ wordpress/wp-content/themes/pfennings/footer.php
@@ -7,11 +7,11 @@
                 Pfenning's Organic Farm<br>
                 1209 Waterloo Street<br>
                 Baden, ON. N3A 1T1<br>
-                <i class="fa fa-fax"></i> +1 (519) 662-4083<br>
-                <a class="button-phone" href="tel://+15196623468"><i class="fa fa-phone-square"></i> +1 (519) 662-3468</a><br>
-                <a class="button-phone" href="tel://8776623468"><i class="fa fa-phone-square"></i> Toll free: 877-662-3468</a><br>
-                <a class="button-email" href="mailto: veggies@pfenningsfarms.ca"><i class="fa fa-envelope"></i> Email</a><br>
-                <a class="button-map" href="https://goo.gl/maps/RnHjJ" target="_blank"><i class="fa fa-map-marker"></i> Map</a>
+                f: +1 (519) 662-4083<br>
+                <a class="button-phone" href="tel://+15196623468">t: +1 (519) 662-3468</a><br>
+                <a class="button-phone" href="tel://8776623468">Toll free: 877-662-3468</a><br>
+                <a class="button-email" href="mailto: veggies@pfenningsfarms.ca">veggies@pfenningsfarms.ca</a><br>
+                <a class="button-map" href="https://goo.gl/maps/RnHjJ" target="_blank">Map</a>
             </p>
         </div>  
 

7b05de06091ac6c31effe6ea3fc8210e81c9eaff
ajay <ajay786singh@gmail.com>
2015-01-29 22:27:08 +0530

    search store add

--- wordpress/wp-content/themes/pfennings/template-request-us.php
+++ wordpress/wp-content/themes/pfennings/template-request-us.php
@@ -1,3 +1,11 @@
 <aside>
+	<div class="store-search">
+		<h4>Look For A Store</h4>
+		<form action="" class="gform_wrapper">
+			<input type="text" name="zipcode" placeholder="Your Postal Code" value="<?php echo $_REQUEST['zipcode'];?>" />
+			<input type="submit" value="Search" >
+		</form>
+	</div>
+	 
 	<?php gravity_form(1, true, true, false, '', true, 12); ?>
 </aside>

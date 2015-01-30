5d3625ed4f33bbe421d4b1c334e33ab5bd38b6f4
ajay <ajay786singh@gmail.com>
2015-01-30 14:31:15 +0530

    mobile menu fix

--- wordpress/wp-content/themes/pfennings/includes/functions/register-menu.php
+++ wordpress/wp-content/themes/pfennings/includes/functions/register-menu.php
@@ -15,4 +15,26 @@ function register_my_menus() {
 
 add_action( 'init', 'register_my_menus' );
 
+class Walker_MM_Menu extends Walker {
+
+    // Tell Walker where to inherit it's parent and id values
+    var $db_fields = array(
+        'parent' => 'menu_item_parent', 
+        'id'     => 'db_id' 
+    );
+
+    /**
+     * At the start of each element, output a <li> and <a> tag structure.
+     * 
+     * Note: Menu objects include url and title properties, so we will use those.
+     */
+    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+        $output .= sprintf( "\n<li class='mm-menu__item'><a class='mm-menu__link' href='%s'%s>%s</a></li>\n",
+            $item->url,
+            ( $item->object_id === get_the_ID() ) ? ' class="current"' : '',
+            $item->title
+        );
+    }
+
+}
 ?>

174dbe56b562e1a616d9b3c4e941df9457b99696
Lionel Mann <lionelmann@gmail.com>
2015-01-30 11:50:41 -0500

    post thumbnail

--- wordpress/wp-content/themes/pfennings/single.php
+++ wordpress/wp-content/themes/pfennings/single.php
@@ -4,21 +4,15 @@
 <?php if(have_posts()): while(have_posts()): the_post();?>
 
 <!-- Get custom meta values -->
-<?php 
-    $bannerHeadline     = wpautop(get_post_meta($post->ID,'_banner_heading',true));
-    $bannerImageId      = get_post_meta($post->ID, '_banner_image', true);
-    $bannerImageUrl     = wp_get_attachment_image_src($bannerImageId,'banner', true);
+<?php
+    $postThumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'banner' );
 ?>
 
-<section role="slider" style="background: url(<?php echo $bannerImageUrl[0]; ?>) no-repeat; background-size: cover; background-position: center center;">
+<section role="slider" style="background: url(<?php echo $postThumb['0']; ?>) no-repeat; background-size: cover; background-position: center center;">
     <header>
         <hgroup>
-            <h6 class="headline"> 
-                <?php if ($bannerHeadline) { ?>
-                    <?php echo $bannerHeadline; ?>
-                <?php } else { ?>
-                    <?php echo the_title(); ?>
-                <?php  } ?> 
+            <h6 class="headline">              
+                <?php echo the_title(); ?>
             </h6>
         </hgroup>
     </header>

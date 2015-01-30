473a7a050c8e8d59cdb4fb64d6bbde2f8af2e063
Lionel Mann <lionelmann@gmail.com>
2015-01-30 16:10:54 -0500

    initial order steps

---
+++ wordpress/wp-content/themes/pfennings/template-order.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Template Name: Order
+*/
+get_header(); ?>
+
+<!-- Loop starts -->
+<?php if(have_posts()):while(have_posts()):the_post();?>
+
+<!-- Get custom meta values -->
+<?php 
+    $bannerHeadline     = wpautop(get_post_meta($post->ID,'_banner_heading',true));
+    $bannerImageId      = get_post_meta($post->ID, '_banner_image', true);
+    $bannerImageUrl     = wp_get_attachment_image_src($bannerImageId,'banner', true);
+?>
+
+<section role="slider" style="background: url(<?php echo $bannerImageUrl[0]; ?>) no-repeat; background-size: cover; background-position: center center;">
+    <header>
+        <hgroup>
+            <h6 class="headline"> 
+                <?php if ($bannerHeadline) { ?>
+                    <?php echo $bannerHeadline; ?>
+                <?php } else { ?>
+                    <?php echo the_title(); ?>
+                <?php  } ?> 
+            </h6>
+        </hgroup>
+    </header>
+</section>
+
+<div class="down-arrow">
+    <a id="down-link" href="#content" class="target"><i class="fa fa-chevron-down"></i></a>
+</div>
+
+<section role="main">
+
+<h3>To set up an account</h3>
+<p>Please <a href="/team/staff">contact our sales team</a> to find out more about buying our produce.<p>
+<h3>Already have an account? Send us your order</h3>
+
+<div class="content" style="width:33%; float: left;">
+<div style="float:left">
+<h5>Step 1</h5>
+<span style="float: left; padding-right: 1em;">
+<i class="fa fa-cloud-download fa-5x"></i>
+</span>
+<p>To get started, download our <a href="/wordpress/wp-content/uploads/2015/01/currentproductlist.pdf">order form</a> (pdf).</p>
+</div>
+</div>
+
+<div class="content" style="width:33%; float: left;">
+<div style="float:left">
+<h5>Step 2</h5>
+<span style="float: left; padding-right: 1em;">
+<i class="fa fa-print fa-5x"></i>
+</span>
+<p>Print the form and fill in your order.</p>
+</div>
+</div>
+
+<div class="content" style="width:33%; float: left;">
+<div style="float:left">
+<h5>Step 3</h5>
+<span style="float: left; padding-right: 1em;">
+<i class="fa fa-check-square fa-5x"></i>
+</span>
+<p>Submit completed forms to:
+<ul>
+<li>Email: veggies@pfenningsfarms.ca</li>
+<li>Phone: 519-662-3468. Toll FREE 877-662-3468</li>
+<li>Fax: 519-662-4083</li>
+</ul>
+</p>
+</div>
+</div>
+<article>
+    <?php the_content(); ?>
+    </article>
+</section>
+
+<?php 
+    endwhile; endif;
+?>
+
+<?php get_footer(); ?>

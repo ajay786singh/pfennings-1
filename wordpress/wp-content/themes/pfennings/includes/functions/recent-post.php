b06615764d6bfaa8fc8363c0c083a7560cffe498
ajay <ajay786singh@gmail.com>
2015-03-04 22:10:24 +0530

    fixes #98

--- wordpress/wp-content/themes/pfennings/includes/functions/recent-post.php
+++ wordpress/wp-content/themes/pfennings/includes/functions/recent-post.php
@@ -6,7 +6,7 @@
 */
 	
 function recentposts() {
-	$args = array( 'numberposts' => '5' );
+	$args = array( 'numberposts' => '5','post_status'=>'publish' );
 	$recent_posts = wp_get_recent_posts( $args );
 	echo '<ul>';
 	foreach( $recent_posts as $recent ){

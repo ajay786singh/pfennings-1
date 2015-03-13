32eac74d011bcdfe3413db61c1111a9569119a65
Sam Chun <schun80@gmail.com>
2015-03-13 17:46:31 -0400

    more chages to order produce page

--- wordpress/wp-content/themes/pfennings/template-order.php
+++ wordpress/wp-content/themes/pfennings/template-order.php
@@ -33,17 +33,20 @@ get_header(); ?>
 </div>
 
 <section role="main">
+	<div class="setUpAccount">
+		<h3>To set up an account</h3>
+		<p>Please <a href="/team/staff">contact our sales team</a> to find out more about buying our produce.<p>
+		<h3>Already have an account? </br> Send us your order</h3>
+	</div>	
 
-	<h3>To set up an account</h3>
-	<p>Please <a href="/team/staff">contact our sales team</a> to find out more about buying our produce.<p>
-	<h3>Already have an account? </br> Send us your order</h3>
+
 	<div class="order-form-container">
 		<div class="content">
 			<div class="step-box">
 				<span class="icon"><i class="fa fa-cloud-download fa-5x"></i></span>
 				<h6>Step 1</h6>
 				<p class="big">download</p>
-				<p>To get started, download our order form</p>
+				<p>To get started, download the current <a href=" http://pfenningsfarms.ca/wordpress/wp-content/uploads/2015/02/pfennings-productlist-feb192015.pdf">order form.</a></p>
 			</div>
 		</div>
 
@@ -65,13 +68,18 @@ get_header(); ?>
 				</span>
 				<h6>Step 3</h6>
 				<p class="big">submit</p>
-				<p>Submit completed forms</p>
+				<p>You can give us your order by email, phone or fax</p>
+				<ul class="submitOptions">
+					<li>Web savvy: Email the completed form to <a href="mailto:veggies@pfenningsfarms.ca">veggies@pfenningsfarms.ca</a></li>
+					<li>Chatty Cathy?: Order by phone: 519-662-3468. Toll Free: 877-662-3488</li>
+					<li>Old school? Us too. Order by fax: 519-662-4083</li>
+				</ul>
 			</div>
 		</div>
 	</div>
-<article>
-    <?php the_content(); ?>
-    </article>
+	<article class="orderProduce">
+	    <?php the_content(); ?>
+	</article>
 </section>
 
 <?php 

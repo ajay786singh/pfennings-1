7f572eba0c3b66788f4f269feacbd22167b6dd4e
ajay <ajay786singh@gmail.com>
2015-04-06 19:52:00 +0530

    fixes #156

--- wordpress/wp-content/themes/pfennings/src/js/custom/sticky-header.js
+++ wordpress/wp-content/themes/pfennings/src/js/custom/sticky-header.js
@@ -1,15 +1,16 @@
 jQuery(function(){
  var stickHeader = 70;
-  jQuery(window).scroll(function() {
-    var scroll = getCurrentScroll();
-      if ( scroll >= stickHeader ) {
-           jQuery('section[role="banner"] header').addClass('sticky');
-        }
-        else {
-            jQuery('section[role="banner"] header').removeClass('sticky');
-        }
-  });
-function getCurrentScroll() {
-    return window.pageYOffset || document.documentElement.scrollTop;
-    }
+	alert($('body').attr('class'));
+  // jQuery(window).scroll(function() {
+    // var scroll = getCurrentScroll();
+      // if ( scroll >= stickHeader ) {
+           // jQuery('section[role="banner"] header').addClass('sticky');
+        // }
+        // else {
+            // jQuery('section[role="banner"] header').removeClass('sticky');
+        // }
+  // });
+// function getCurrentScroll() {
+    // return window.pageYOffset || document.documentElement.scrollTop;
+    // }
 });

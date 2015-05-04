55e9f850f5b7407eb0e17b15c95f39363ee77c15
ajay <ajay786singh@gmail.com>
2015-05-04 15:24:18 +0530

    404 error page fix

--- wordpress/wp-content/themes/pfennings/404.php
+++ wordpress/wp-content/themes/pfennings/404.php
@@ -1,10 +1,18 @@
 <?php get_header(); ?>
-
-<section role="main">
-    <article>
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>  
-   </article>
+<section role="slider" style="background-image: url('');">
+    <header>
+        <hgroup>
+            <h6 class="headline">Page Not Found</h6>
+        </hgroup>
+    </header>
 </section>
-
+<div class="down-arrow">
+    <a id="down-link" href="#content" class="target"><i class="fa fa-chevron-down"></i></a>
+</div>
+<section role="main">
+    <div class="post-container center-content">
+       <h5>Oops!</h5>
+	   <p>We are really sorry but the page you requested cannot be found.</p>
+   </div>
+</section>
 <?php get_footer(); ?>

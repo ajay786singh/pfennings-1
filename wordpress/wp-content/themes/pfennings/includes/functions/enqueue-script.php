522948aee462c261ccbae4d3e9d78a7e887b80e4
ajay <ajay786singh@gmail.com>
2015-05-04 16:40:51 +0530

    fb share fix

--- wordpress/wp-content/themes/pfennings/includes/functions/enqueue-script.php
+++ wordpress/wp-content/themes/pfennings/includes/functions/enqueue-script.php
@@ -6,8 +6,8 @@
 
 function my_scripts() {
 	wp_enqueue_script('jquery');
-	wp_enqueue_script( 'map', 'http://maps.google.com/maps/api/js?sensor=true', array(), '1.0.0', false);
-	wp_enqueue_script( 'app', get_template_directory_uri() . '/dist/js/app.min.js', array(), '1.0.0', false);
+	wp_enqueue_script( 'map', 'http://maps.google.com/maps/api/js?sensor=true', array(), '1.0.0', true);
+	wp_enqueue_script( 'app', get_template_directory_uri() . '/dist/js/app.min.js', array(), '1.0.0', true);
 }
 
 add_action( 'wp_enqueue_scripts', 'my_scripts' );
